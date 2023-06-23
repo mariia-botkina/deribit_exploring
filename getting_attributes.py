6fa094d85dc93868938e0c8d7c5a920bcbe1239b
mariia-botkina <104559877+mariia-botkina@users.noreply.github.com>
2023-06-23 10:27:00 +0300

    As a result, we get a list of dataclasses

--- getting_attributes.py
+++ getting_attributes.py
@@ -1,42 +1,6 @@
-from typing import Dict
-from dataclasses import dataclass
+from typing import Dict, List
 import requests
 import json
-
-
-@dataclass
-class SymbolData:
-    tick_size_steps: list
-    tick_size: float
-    taker_commission: float
-    strike: float
-    settlement_period: str
-    settlement_currency: str
-    rfq: bool
-    quote_currency: str
-    price_index: str
-    option_type: str
-    min_trade_amount: float
-    maker_commission: float
-    kind: str
-    is_active: bool
-    instrument_type: str
-    instrument_name: str
-    instrument_id: int
-    expiration_timestamp: int
-    creation_timestamp: int
-    counter_currency: str
-    contract_size: float
-    block_trade_tick_size: float
-    block_trade_min_trade_amount: float
-    block_trade_commission: float
-    base_currency: str
-    max_liquidation_commission: float
-    max_leverage: int
-    future_type: str
-    block_trade_min_trade_amount: int
-    taker_commission: int
-    maker_commission: int
 
 
 def get_type(attribute_type: type):
@@ -52,9 +16,9 @@ def get_all_request_attributes(dictionary: list[Dict]):
         for key in Dictionary.keys():
             key_type = get_type(type(Dictionary[key]))
             with open('request_attributes.txt', 'r+') as f:
-                SymbolData = ' '.join(f.readlines())
+                dictionary_keys = ' '.join(f.readlines())
 
-            if f'{key}: {key_type}' not in SymbolData:
+            if f'{key}: {key_type}' not in dictionary_keys:
                 with open('request_attributes.txt', 'a') as f:
                     f.writelines(f'{key}: {key_type}\n')
 
@@ -62,5 +26,9 @@ def get_all_request_attributes(dictionary: list[Dict]):
 if __name__ == '__main__':
     res = requests.get(
         "https://test.deribit.com/api/v2/public/get_instruments?currency=BTC&settlement_period=perpetual")
-    result: list = json.loads(res.text)['result']
+    result: List = json.loads(res.text)['result']
     get_all_request_attributes(result)
+
+    # fields = [f'{item.name}: Optional[{get_type(item.type)}]' for item in fields(SymbolData)]
+    # print('\n'.join(fields))
+    a = 0
