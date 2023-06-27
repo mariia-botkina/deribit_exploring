aa8cc22e62fabb549f2615bfe4a9c99ca46f4b40
mariia-botkina <104559877+mariia-botkina@users.noreply.github.com>
2023-06-28 00:24:25 +0300

    Added a option request

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from typing import List
+from typing import List, Dict, Union
 import requests
 import json
 
-from utils import get_list_of_dataclasses
+from requests import Response
 
+from model import SymbolData
+from utils import get_list_of_symbols_data
 
 if __name__ == '__main__':
-    res = requests.get(
-        "https://test.deribit.com/api/v2/public/get_instruments?currency=BTC&settlement_period=perpetual")
-    result: List = json.loads(res.text)['result']
+    url = "https://deribit.com/api/v2/public/get_instruments"
+    params: Dict[str, str] = {
+        'currency': 'BTC',
+        'kind': 'option',
+    }
 
-    list_of_dataclasses = get_list_of_dataclasses(result)
+    res: Response = requests.get(url=url, params=params)
+    result: List[Dict[str, Union[int, float, bool, str]]] = json.loads(res.text)['result']
+
+    symbols_info: List[SymbolData] = get_list_of_symbols_data(result)
+
+    url_option = 'https://test.deribit.com/api/v2/public/get_book_summary_by_currency'
+    params_option: Dict[str, str] = {
+        'currency': 'BTC',
+        'kind': 'option',
+
+    }
+
+    opt_cost_request: Response = requests.get(url=url_option, params=params_option)
+    option_cost: List[Dict[str, Union[int, float, bool, str]]] = json.loads(opt_cost_request.text)['result']
 
     a = 1
