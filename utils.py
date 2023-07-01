c1c25c022136ee3384b2449b24584e8edfdfcf2b
mariia-botkina <104559877+mariia-botkina@users.noreply.github.com>
2023-07-01 23:34:09 +0300

    Added type indication

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from dataclasses import fields
-from typing import Tuple, List
+from typing import Tuple, List, Dict, Any
 
 from model import SymbolData
 
@@ -8,11 +8,14 @@ def get_names_of_attributes(my_tuple: Tuple):
     return [item.name for item in my_tuple]
 
 
-def get_list_of_dataclasses(list_of_dictionaries: List):
-    list_of_dataclasses = []
+def get_list_of_symbols_data(list_of_dictionaries: List[Dict[str, Any]]) -> List[SymbolData]:
+    list_of_symbols_data: List[SymbolData] = []
+
     for dictionary in list_of_dictionaries:
-        my_dict = dict.fromkeys(get_names_of_attributes(fields(SymbolData)))
+        symbol_data__dict: Dict[str, Any] = dict.fromkeys(get_names_of_attributes(fields(SymbolData)))
         for key in dictionary.keys():
-            my_dict[key] = dictionary[key]
-        list_of_dataclasses.append(SymbolData(*my_dict.values()))
-    return list_of_dataclasses
+            symbol_data__dict[key] = dictionary[key]
+
+        list_of_symbols_data.append(SymbolData(*symbol_data__dict.values()))
+
+    return list_of_symbols_data
