8e02c91626e308173ea59be28c56ac6024bf986e
mariia-botkina <104559877+mariia-botkina@users.noreply.github.com>
2023-07-04 18:18:12 +0300

    Creation options_info dictionary

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from collections import defaultdict
-from dataclasses import dataclass
-from typing import List, Dict, Union, Optional
-import requests
 import json
+from collections import defaultdict
+from typing import List, Dict, Union
 
+import requests
 from requests import Response
 
-from model import SymbolData
-from utils import get_list_of_symbols_data
+from model import SymbolData, OptionData
+from utils import get_list_of_symbols_data, get_list_of_call_and_put_data, create_option_data
 
 if __name__ == '__main__':
     url = "https://deribit.com/api/v2/public/get_instruments"
@@ -20,49 +19,30 @@ if __name__ == '__main__':
     result: List[Dict[str, Union[int, float, bool, str]]] = json.loads(res.text)['result']
 
     symbols_info: List[SymbolData] = get_list_of_symbols_data(result)
+    
+    symbols_data: Dict[str, Dict[str, Union[int, float, None]]] = defaultdict(defaultdict)
+    for symbol in result:
+        series = symbol['instrument_name'][:-2]
+        symbols_data[series]['creation_timestamp'] = symbol['creation_timestamp']
+        symbols_data[series]['expiration_timestamp'] = symbol['expiration_timestamp']
+        symbols_data[series]['strike'] = symbol['strike']
 
     url_option = 'https://deribit.com/api/v2/public/get_book_summary_by_currency'
     params_option: Dict[str, str] = {
         'currency': 'BTC',
         'kind': 'option',
-
     }
 
     opt_prices_request: Response = requests.get(url=url_option, params=params_option)
     options_prices: List[Dict[str, Union[int, float, bool, str]]] = json.loads(opt_prices_request.text)['result']
 
-    ts_data: Dict[str, List[int]] = defaultdict(list)
+    options_call, options_put = get_list_of_call_and_put_data(options_prices=options_prices)
 
-    for data in options_prices:
-        series = data['instrument_name'].split('-')[0] + data['instrument_name'].split('-')[1]
-        ts_data[series].append(data['creation_timestamp'])
-
-    """
-        BTC-29JUN23-30000-P
-        30000 - strike
-        P - put
-    """
-
-
-    @dataclass
-    class OptionData:
-        maturity: float  # expiry - creation_timestamp -> years
-        underlying_price: float  # from prices
-        ask_price: Optional[float]  # min from put, call
-        ask_type: str  # put or call
-        bid_price: Optional[float]  # max from put, call
-        bid_type: str  # put or call
-        strike: int  # from symbols info
-        name: str  # BTC-29JUN23-30000
-
-
-    ot_data: Dict[str, List[int]] = defaultdict(list)
-
-    for data in result:
-        series = data['instrument_name'].split('-')[0] + data['instrument_name'].split('-')[1]
-        ot_data[series].append(data['option_type'])
-
-    for series, ts_list in ts_data.items():
-        print(series, max(ts_list) - min(ts_list), ot_data[series])
+    options_info: Dict[str, List[OptionData]] = defaultdict(list)
+    for call, put in zip(options_call, options_put):
+        series = call['instrument_name'].split('-')[0] + call['instrument_name'].split('-')[1]
+        option = create_option_data(call=call, put=put, symbols_data=symbols_data)
+        if option is not None:
+            options_info[series].append(option)
 
     a = 1
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from dataclasses import fields
-from typing import Tuple, List, Dict, Any
+from typing import Tuple, List, Dict, Any, Union
 
-from model import SymbolData
+from model import SymbolData, OptionData
 
 
 def get_names_of_attributes(my_tuple: Tuple):
@@ -12,10 +12,71 @@ def get_list_of_symbols_data(list_of_dictionaries: List[Dict[str, Any]]) -> List
     list_of_symbols_data: List[SymbolData] = []
 
     for dictionary in list_of_dictionaries:
-        symbol_data__dict: Dict[str, Any] = dict.fromkeys(get_names_of_attributes(fields(SymbolData)))
+        symbol_data_dict: Dict[str, Any] = dict.fromkeys(get_names_of_attributes(fields(SymbolData)))
         for key in dictionary.keys():
-            symbol_data__dict[key] = dictionary[key]
+            symbol_data_dict[key] = dictionary[key]
 
-        list_of_symbols_data.append(SymbolData(*symbol_data__dict.values()))
+        list_of_symbols_data.append(SymbolData(*symbol_data_dict.values()))
 
     return list_of_symbols_data
+
+
+def get_list_of_call_and_put_data(options_prices: List[Dict[str, Union[int, float, bool, str]]]) \
+        -> Tuple[List[Dict[str, Union[int, float, bool, str]]], List[Dict[str, Union[int, float, bool, str]]]]:
+    options_call: List[Dict[str, Union[int, float, bool, str]]] = []
+    options_put: List[Dict[str, Union[int, float, bool, str]]] = []
+    for option in options_prices:
+        if option['instrument_name'][-1] == 'C':
+            options_call.append(option)
+        else:
+            options_put.append(option)
+
+    options_call.sort(key=lambda x: x['instrument_name'])
+    options_put.sort(key=lambda x: x['instrument_name'])
+    return options_call, options_put
+
+
+def create_option_data(call: Dict[str, Union[int, float, bool, str]], put: Dict[str, Union[int, float, bool, str]],
+                       symbols_data: Dict[str, Dict[str, Union[int, float, None]]]) -> Union[None, OptionData]:
+    option_data_dict: Dict[str, Any] = dict.fromkeys(get_names_of_attributes(fields(OptionData)))
+
+    if call['ask_price'] is not None and put['ask_price'] is not None:
+        option_data_dict['ask_price'] = min(call['ask_price'], put['ask_price'])
+        if option_data_dict['ask_price'] is call['ask_price']:
+            option_data_dict['ask_type'] = 'call'
+        else:
+            option_data_dict['ask_type'] = 'put'
+    elif call['ask_price'] is None and put['ask_price'] is None:
+        return None
+    else:
+        match call['ask_price']:
+            case None:
+                option_data_dict['ask_price'] = put['ask_price']
+                option_data_dict['ask_type'] = 'put'
+            case _:
+                option_data_dict['ask_price'] = call['ask_price']
+                option_data_dict['ask_type'] = 'call'
+
+    if call['bid_price'] is not None and put['bid_price'] is not None:
+        option_data_dict['bid_price'] = min(call['bid_price'], put['bid_price'])
+        if option_data_dict['bid_price'] is call['bid_price']:
+            option_data_dict['bid_type'] = 'call'
+        else:
+            option_data_dict['bid_type'] = 'put'
+    elif call['bid_price'] is None and put['bid_price'] is None:
+        return None
+    else:
+        match call['bid_price']:
+            case None:
+                option_data_dict['bid_price'] = put['bid_price']
+                option_data_dict['bid_type'] = 'put'
+            case _:
+                option_data_dict['bid_price'] = call['bid_price']
+                option_data_dict['bid_type'] = 'call'
+
+    option_data_dict['name'] = call['instrument_name'][:-2]
+    option_data_dict['strike'] = symbols_data[option_data_dict['name']]['strike']
+    option_data_dict['maturity'] = (symbols_data[option_data_dict['name']]['expiration_timestamp'] - symbols_data[option_data_dict['name']]['creation_timestamp']) / 31536000000
+    option_data_dict['underlying_price'] = call['underlying_price']
+
+    return OptionData(*option_data_dict.values())
